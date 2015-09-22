1344943eb35cb34e35dcc8d72ab92225aa205250
codeGlider <hjw2012607@163.com>
2015-09-22 11:31:39 +0800

    删除之前登陆页面，自定义新的登陆页面

---
+++ TuringChatMachine/LoginViewController.swift
@@ -0,0 +1,81 @@
+//
+//  LoginViewController.swift
+//  ParseDemo
+//
+//  Created by Rumiya Murtazina on 7/28/15.
+//  Copyright (c) 2015 abearablecode. All rights reserved.
+//
+
+import UIKit
+import Parse
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBAction func unwindToLogInScreen(segue:UIStoryboardSegue) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        password.secureTextEntry = true
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func loginAction(sender: UIButton) {
+        let username = self.username.text
+        let password = self.password.text
+        
+        guard username?.getLength() >= 4 else{
+            let alert = UIAlertView(title: "登陆错误", message: "用户名长度必须大于4个字符", delegate: self, cancelButtonTitle: "确定")
+            alert.show()
+            return
+        }
+        guard password?.getLength() >= 8 else{
+            let alert = UIAlertView(title: "登陆错误", message: "密码长度必须大于8个字符", delegate: self, cancelButtonTitle: "确定")
+            alert.show()
+            return
+        }
+        
+        let spinner: UIActivityIndicatorView = UIActivityIndicatorView(frame: CGRectMake(self.view.frame.width/2 - 25,100, 50, 50)) as UIActivityIndicatorView
+        spinner.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.WhiteLarge
+        
+        self.view.addSubview(spinner)
+        spinner.startAnimating()
+        PFUser.logInWithUsernameInBackground(username!, password: password!) { (user,error) -> Void in
+            spinner.stopAnimating()
+            guard user != nil else{
+                let alert = UIAlertView(title: "错误 ", message: "\(error)", delegate: self, cancelButtonTitle: "确定")
+                alert.show()
+                
+                return
+            }
+            let alert = UIAlertView(title: "成功", message: "已登陆", delegate: self, cancelButtonTitle: "确定")
+            alert.show()
+            
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                let viewController:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("Home")
+                self.presentViewController(viewController, animated: true, completion: nil)
+            })
+            
+            
+        }
+        
+        
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TuringChatMachine/SignUpViewController.swift
+++ TuringChatMachine/SignUpViewController.swift
@@ -1,66 +1,78 @@
 //
 //  SignUpViewController.swift
-//  TuringChatMachine
+//  ParseDemo
 //
-//
-//
-//                                          ___                        ___
-//     ___                                 /  /\          ___         /  /\
-//    /  /\                               /  /::\        /__/\       /  /:/_
-//   /  /:/               ___     ___    /  /:/\:\       \  \:\     /  /:/ /\
-//  /__/::\              /__/\   /  /\  /  /:/  \:\       \  \:\   /  /:/ /:/_
-//  \__\/\:\__           \  \:\ /  /:/ /__/:/ \__\:\  ___  \__\:\ /__/:/ /:/ /\
-//     \  \:\/\           \  \:\  /:/  \  \:\ /  /:/ /__/\ |  |:| \  \:\/:/ /:/
-//      \__\::/            \  \:\/:/    \  \:\  /:/  \  \:\|  |:|  \  \::/ /:/
-//      /__/:/              \  \::/      \  \:\/:/    \  \:\__|:|   \  \:\/:/
-//      \__\/                \__\/        \  \::/      \__\::::/     \  \::/
-//                                         \__\/           ~~~~       \__\/
-//       ___          _____          ___                       ___           ___           ___
-//      /  /\        /  /::\        /  /\        ___          /  /\         /__/\         /  /\
-//     /  /::\      /  /:/\:\      /  /::\      /  /\        /  /::\        \  \:\       /  /::\
-//    /  /:/\:\    /  /:/  \:\    /  /:/\:\    /  /:/       /  /:/\:\        \  \:\     /  /:/\:\
-//   /  /:/~/::\  /__/:/ \__\:|  /  /:/~/:/   /__/::\      /  /:/~/::\   _____\__\:\   /  /:/~/::\
-//  /__/:/ /:/\:\ \  \:\ /  /:/ /__/:/ /:/___ \__\/\:\__  /__/:/ /:/\:\ /__/::::::::\ /__/:/ /:/\:\
-//  \  \:\/:/__\/  \  \:\  /:/  \  \:\/:::::/    \  \:\/\ \  \:\/:/__\/ \  \:\~~\~~\/ \  \:\/:/__\/
-//   \  \::/        \  \:\/:/    \  \::/~~~~      \__\::/  \  \::/       \  \:\  ~~~   \  \::/
-//    \  \:\         \  \::/      \  \:\          /__/:/    \  \:\        \  \:\        \  \:\
-//     \  \:\         \__\/        \  \:\         \__\/      \  \:\        \  \:\        \  \:\
-//      \__\/                       \__\/                     \__\/         \__\/         \__\/
-//
-//
-//
-//
-//
-//  Created by Huangjunwei on 15/9/11.
-//  Copyright (c) 2015年 codeGlider. All rights reserved.
+//  Created by Rumiya Murtazina on 7/30/15.
+//  Copyright (c) 2015 abearablecode. All rights reserved.
 //
 
 import UIKit
 import Parse
-import ParseUI
-class SignUpViewController: PFSignUpViewController {
-
+class SignUpViewController: UIViewController {
+    
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-self.signUpView?.logo = UIImageView(image: UIImage(named: "logo"))
-        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func signUpAction(sender: AnyObject) {
+        let username = self.usernameField.text!
+        let password = self.passwordField.text!
+        let email = self.emailField.text!
+        let finalEmail = email.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+        
+        guard username.getLength() >= 5 else{
+            let alert = UIAlertView(title: "错误", message: "用户名必须大于5个字符！", delegate: self, cancelButtonTitle: "确定")
+            alert.show()
+            return
+        }
+        guard password.getLength() >= 8 else{
+            let alert = UIAlertView(title: "错误", message: "密码必须大于5个字符！", delegate: self, cancelButtonTitle: "确定")
+            alert.show()
+            return
+        }
+        guard email.getLength() >= 8 else{
+            let alert = UIAlertView(title: "错误", message: "请输入正确的电子邮箱地址！", delegate: self, cancelButtonTitle: "确定")
+            alert.show()
+            return
+        }
+        let spinner: UIActivityIndicatorView = UIActivityIndicatorView(frame: CGRectMake(0, 0, 150, 150)) as UIActivityIndicatorView
+        spinner.startAnimating()
+        
+        let newUser = PFUser()
+        
+        newUser.username = username
+        newUser.password = password
+        newUser.email = finalEmail
+        
+        // Sign up the user asynchronously
+        newUser.signUpInBackgroundWithBlock({ (succeed, error) -> Void in
+            
+            // Stop the spinner
+            spinner.stopAnimating()
+            guard ((error) != nil) else {
+                let alert = UIAlertView(title: "错误", message: "\(error)", delegate: self, cancelButtonTitle: "确定")
+                alert.show()
+                return
+                
+            }
+            let alert = UIAlertView(title: "成功", message: "已注册！ ", delegate: self, cancelButtonTitle: "确定")
+            alert.show()
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                let viewController:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("Home")
+                self.presentViewController(viewController, animated: true, completion: nil)
+            })
+            
+        })
     }
-    */
-
+    
 }
+
